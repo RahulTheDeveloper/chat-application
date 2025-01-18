ac9aafc9cef3f49e36e031891994eaba3314be42
RahulTheDeveloper <rahulkrkashyap9899@gmail.com>
2025-01-18 15:27:06 +0530

    Update fiile2.java
    ho gya

--- fiile2.java
+++ fiile2.java
@@ -1,6 +1,6 @@
 public class Main {
     public static int main(String[] args) {
-        int num = 123;
+        int num = 789;
         String s = Integer.toString(num); // Integer ko string mein convert kiya
         System.out.println(s + " is now a string");
     }
