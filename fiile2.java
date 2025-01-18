2d855d430572b0baac5d2498705f95629e0629ae
RahulTheDeveloper <rahulkrkashyap9899@gmail.com>
2025-01-18 15:14:18 +0530

    Create fiile2.java
    mein ye wala kya hian

---
+++ fiile2.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 123;
+        String s = Integer.toString(num); // Integer ko string mein convert kiya
+        System.out.println(s + " is now a string");
+    }
+}
